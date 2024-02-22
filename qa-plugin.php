55c59624ffce750b7c61ad770a573b6bcfbd1bc4
Chun <heliochun@users.noreply.github.com>
2024-02-22 06:37:00 +0000

    .

--- qa-plugin.php
+++ qa-plugin.php
@@ -1,14 +1,14 @@
 <?php
 
 /*
-	Plugin Name: Show More
-	Plugin URI: https://github.com/HelioChun/q2a-read-more
-	Plugin Update Check URI: https://raw.githubusercontent.com/HelioChun/q2a-read-more/master/qa-plugin.php
+	Plugin Name: Show More Text Button
+	Plugin URI: https://github.com/rxchun/q2a-read-more
+	Plugin Update Check URI: https://raw.githubusercontent.com/rxchun/q2a-read-more/master/qa-plugin.php
 	Plugin Description: Collapses tall blocks of text (questions/answers/comments) and adds a "read more" button
 	Plugin Version: 1.1
 	Plugin Date: 2015-05-06
 	Plugin Author: Chun
-	Plugin Author URI: https://github.com/heliochun
+	Plugin Author URI: https://rxchun.github.io/
 	Plugin License: GPLv2
 	Plugin Minimum Question2Answer Version: 1.5
 */
